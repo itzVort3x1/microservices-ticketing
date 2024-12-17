d8536b6e9990f1ebe6714615b21c111686095eba
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-17 15:44:25 +0000

    updated k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { json } from "body-parser";
+const cors = require("cors");
 
 const app = express();
 app.use(json());
+app.use(cors());
+
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hi there!");
+});
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000!");
+    console.log("Listening on port 3000!!!");
 });

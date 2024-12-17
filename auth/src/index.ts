2a3fd14ee12cfbe3ff4f1aec519b4ff8da5f866c
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-17 20:07:42 +0000

    added routing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,19 @@ import express from "express";
 import { json } from "body-parser";
 const cors = require("cors");
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 app.use(cors());
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!");

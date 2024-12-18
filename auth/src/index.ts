0e4d6dabee6921adbc82fe0df59955a3283f565b
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-18 20:10:12 +0000

    updated auth service and finished mongodb data modeling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 const cors = require("cors");
 
 import { currentUserRouter } from "./routes/current-user";
@@ -25,6 +27,17 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000!!!");
-});
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+
+    app.listen(3000, () => {
+        console.log("Listening on port 3000!!!");
+    });
+};
+
+start();

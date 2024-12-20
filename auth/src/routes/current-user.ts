c96e5521bc721dc0869e86b9baba1fb3b90b5b17
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-20 16:19:08 +0000

    finished adding testing

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response } from "express";
 
 import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 router.get(
     "/api/users/currentuser",
     currentUser,
-    requireAuth,
     (req: Request, res: Response) => {
-        res.send({ currentUser: req.currentUser || null });
+        res.status(200).send({ currentUser: req.currentUser || null });
     }
 );
 

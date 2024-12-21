21986d4f282b6655f7b88366c5e9f0c7cbddf644
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-21 13:52:14 +0000

    updated signup logic

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import axios from "axios";
+
 const signup = () => {
-    return <h1>Signup Page</h1>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await axios.post("/api/users/signup", {
+            email,
+            password,
+        });
+
+        console.log(response.data);
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign Up</h1>
+            <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    className="form-control"
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    className="form-control"
+                />
+            </div>
+            <button className="btn btn-primary">Sign Up</button>
+        </form>
+    );
 };
+
+export default signup;

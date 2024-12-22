ed6bcb817f0f3e1e4d3c77a7752b84143e1333b3
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-22 19:22:04 +0000

    'updated imports and added npm library

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@kaustubhtech/common";
 import { Password } from "../services/password";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 

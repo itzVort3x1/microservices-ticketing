9c861930b440d7e89f2cd43ee2f79af38276bc46
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-27 14:26:36 +0000

    added listening service

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,28 @@
+import { Message } from "node-nats-streaming";
+import {
+    Subjects,
+    Listener,
+    TicketUpdatedEvent,
+    NotFoundError,
+} from "@kaustubhtech/common";
+import { Ticket } from "../../models/tickets";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    readonly subject = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+        const ticket = await Ticket.findById(data.id);
+
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        const { title, price } = data;
+        ticket.set({ title, price });
+        await ticket.save();
+
+        msg.ack();
+    }
+}

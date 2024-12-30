27acf8b3f12d6c4d25233de5bf95eab0d1715586
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-30 11:28:03 +0000

    added test cases of strip payment

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import { app } from "../../app";
 import { Order } from "../../models/order";
 import mongoose from "mongoose";
 import { OrderStatus } from "@kaustubhtech/common";
+import { stripe } from "../../stripe";
+
+jest.mock("../../stripe");
 
 describe("GET /api/payments", () => {
     it("returns a 404 when purchasing an order that does not exist", async () => {
@@ -58,5 +61,38 @@ describe("GET /api/payments", () => {
                 orderId: order.id,
             })
             .expect(400);
+    });
+
+    it("returns a 201 with valid inputs", async () => {
+        const userId = new mongoose.Types.ObjectId().toHexString();
+        const price = Math.floor(Math.random() * 100000);
+
+        const order = Order.build({
+            id: new mongoose.Types.ObjectId().toHexString(),
+            userId,
+            version: 0,
+            price,
+            status: OrderStatus.Created,
+        });
+
+        await order.save();
+
+        await request(app)
+            .post("/api/payments")
+            .set("Cookie", global.signin(userId))
+            .send({
+                token: "tok_visa",
+                orderId: order.id,
+            })
+            .expect(201);
+
+        const stripeCharges = await stripe.charges.list({ limit: 50 });
+
+        const stripeCharge = stripeCharges.data.find((charge) => {
+            return charge.amount === price * 100;
+        });
+
+        expect(stripeCharge).toBeDefined();
+        expect(stripeCharge!.currency).toEqual("usd");
     });
 });
